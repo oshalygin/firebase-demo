8ef6f482151762697dcff95ed71fe7d9b3bced4a
Oleg Shalygin <oshalygin@gmail.com>
2016-11-08 16:37:05 -0800

    Add player and game objects

---
+++ assets/application.js
@@ -0,0 +1,21 @@
+let gameData = {
+    inGame: false,
+    playerOneLocked: false,
+    playerTwoLocked: false,
+    players: 0
+};
+
+let playerOne = {
+    choice: "",
+    name: "",
+    losses: 0,
+    wins: 0
+};
+
+let playerTwo = {
+    choice: "",
+    name: "",
+    losses: 0,
+    wins: 0
+};
+
