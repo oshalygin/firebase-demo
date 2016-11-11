bf035d13696802b928d62adade634381299ae247
Oleg Shalygin <oshalygin@gmail.com>
2016-11-10 21:10:04 -0800

    Add firebase wireup

--- assets/application.js
+++ assets/application.js
@@ -1,21 +1,49 @@
-let gameData = {
+const gamePath = "/game";
+const playerOnePath = "/players/playerOne";
+const playerTwoPath = "/game";
+
+function initializeGameDetails(database, gameData, playerOne, playerTwo) {
+    database.ref(gamePath).set(gameData);
+    database.ref(playerOnePath).set(playerOne);
+    database.ref(playerTwoPath).set(playerTwo);
+}
+
+function printPlayerOneDetails(database) {
+    database.ref(playerOnePath).on("value", (snapshot) => {
+        console.log(snapshot.val());
+    });
+}
+
+function printPlayerTwoDetails(database) {
+    database.ref(playerTwoPath).on("value", (snapshot) => {
+        console.log(snapshot.val());
+    });
+}
+
+
+const gameData = {
     inGame: false,
     playerOneLocked: false,
     playerTwoLocked: false,
     players: 0
 };
 
-let playerOne = {
+const playerOne = {
     choice: "",
     name: "",
     losses: 0,
     wins: 0
 };
 
-let playerTwo = {
+const playerTwo = {
     choice: "",
     name: "",
     losses: 0,
     wins: 0
 };
 
+
+const database = firebase.database(); //eslint-disable-line no-undef
+initializeGameDetails(database, gameData, playerOne, playerTwo);
+printPlayerOneDetails(database);
+printPlayerTwoDetails(database);
